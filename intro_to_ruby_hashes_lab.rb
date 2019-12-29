e0a91abb6080ae9d2a2137076c921c9e76281b3a
Corey Hodge <contact@coreyhodge.net>
2019-12-29 02:08:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-    ripeFoodHash = {id: 3}
+    totalIdsDistributed = {id: 3}
 end
 
 def my_hash_creator(key, value)
