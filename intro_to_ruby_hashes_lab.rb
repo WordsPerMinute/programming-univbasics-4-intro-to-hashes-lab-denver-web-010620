74a0f8dad7268dc2cf7ec5eff54bd53f7cabac6c
Corey Hodge <contact@coreyhodge.net>
2019-12-29 02:14:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ end
 
 def my_hash_creator(name, value)
   # return a hash that includes the key and value parameters passed into this method
-    passed_hash = {name: value}
+    passed_hash = {name => value}
 end
 
 def read_from_hash(hash, key)
